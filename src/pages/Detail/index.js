9109b5082924ccf78a67bc162d5f0e25a901ef99
phat nguyen <phat.nguyen@terralogic.com>
2020-08-25 10:01:40 +0700

    no message

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -18,7 +18,7 @@ class Detail extends React.Component {
       complete: false,
       id: "",
       timerStart: 0,
-      timerTime: 0,
+      timerTime: 0
     };
   }
 
@@ -27,13 +27,13 @@ class Detail extends React.Component {
       match: { params: { id = "" } = {} } = {},
       getById,
       getListComment = () => {},
-      getListMostPopular,
+      getListMostPopular
     } = this.props;
     getById(id);
     getListComment(id);
     getListMostPopular({});
     this.setState({
-      id,
+      id
     });
   }
 
@@ -42,7 +42,7 @@ class Detail extends React.Component {
       match: { params: { id: idParams = "" } = {} } = {},
       getById,
       getListComment = () => {},
-      itemMediaSocial: { timeForRecvCoin } = {},
+      itemMediaSocial: { timeForRecvCoin } = {}
     } = this.props;
     const { id, timerTime, complete } = this.state;
     if (id !== idParams) {
@@ -52,7 +52,7 @@ class Detail extends React.Component {
         id: idParams,
         complete: false,
         timerStart: 0,
-        timerTime: 0,
+        timerTime: 0
       });
       this.stopTimer();
     }
@@ -65,23 +65,23 @@ class Detail extends React.Component {
   componentWillUnmount() {
     const { updateStateReducer, updateStateCommentReducer } = this.props;
     updateStateReducer({
-      itemMediaSocial: {},
+      itemMediaSocial: {}
     });
     updateStateCommentReducer({
       listComment: [],
       paging: {},
-      messageErrorComment: "",
+      messageErrorComment: ""
     });
   }
 
   startTimer = () => {
     this.setState({
       timerTime: this.state.timerTime,
-      timerStart: Date.now() - this.state.timerTime,
+      timerStart: Date.now() - this.state.timerTime
     });
     this.timer = setInterval(() => {
       this.setState({
-        timerTime: Date.now() - this.state.timerStart,
+        timerTime: Date.now() - this.state.timerStart
       });
     }, 10);
   };
@@ -90,19 +90,19 @@ class Detail extends React.Component {
     clearInterval(this.timer);
   };
 
-  handleAddComment = (comment) => {
+  handleAddComment = comment => {
     const { addComment, itemMediaSocial: { id = "" } = {} } = this.props;
     const payload = {
       postId: id,
       comment,
-      title: "My Comment",
+      title: "My Comment"
     };
     addComment(payload);
   };
 
   checkComplete = () => {
     this.setState({
-      complete: true,
+      complete: true
     });
   };
 
@@ -117,7 +117,7 @@ class Detail extends React.Component {
       loadingAction,
       // actionSuccessfully,
       // loadingMostPopular,
-      listMostPopular = [],
+      listMostPopular = []
     } = this.props;
     const {
       name = "",
@@ -125,7 +125,7 @@ class Detail extends React.Component {
       countView = 0,
       authorId = "",
       timeForRecvCoin,
-      pointForUserView,
+      pointForUserView
     } = itemMediaSocial;
     const { complete, id, timerTime } = this.state;
     let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
@@ -137,17 +137,15 @@ class Detail extends React.Component {
     }
     const stringCountView = numeral(countView).format("0,0");
     const itemChanel =
-      listUserData.find((element) => element.userId === authorId) || {};
-    const formatListVideoMostPopular = listMostPopular.map((item) => {
+      listUserData.find(element => element.userId === authorId) || {};
+    const formatListVideoMostPopular = listMostPopular.map(item => {
       return {
         ...item,
-        author: listUserData.find(
-          (element) => element.userId === item.authorId
-        ),
+        author: listUserData.find(element => element.userId === item.authorId)
       };
     });
     const filterListVideoMostPopular = formatListVideoMostPopular.filter(
-      (item) => item.id !== id
+      item => item.id !== id
     );
     return (
       <div className="container__detail">
@@ -160,7 +158,7 @@ class Detail extends React.Component {
           />
           <div className="rightList" style={{ height: "315px" }}>
             <div className="scrollView">
-              {filterListVideoMostPopular.map((item) => (
+              {filterListVideoMostPopular.map(item => (
                 <ItemVideoVertical key={item.id} item={item} />
               ))}
             </div>
@@ -230,7 +228,7 @@ class Detail extends React.Component {
             className="rightList"
             style={{ backgroundColor: "transparent", boxShadow: "none" }}
           >
-            {filterListVideoMostPopular.map((item) => (
+            {filterListVideoMostPopular.map(item => (
               <ItemVideoVertical key={item.id} item={item} />
             ))}
           </div>
@@ -239,6 +237,7 @@ class Detail extends React.Component {
           <Notification
             status="success"
             message=""
+            timeout={10000}
             title={`You receive ${pointForUserView} CXC`}
           />
         )}
@@ -254,10 +253,10 @@ const mapStateToProps = ({
     // paging {},
     messageErrorComment = "",
     loadingAction = false,
-    actionSuccessfully = "",
+    actionSuccessfully = ""
   } = {},
   listUser: { listUserData = [] } = {},
-  mostPopular: { loading: loadingMostPopular, listMostPopular = [] } = {},
+  mostPopular: { loading: loadingMostPopular, listMostPopular = [] } = {}
 }) => ({
   itemMediaSocial,
   loadingGetById,
@@ -269,21 +268,20 @@ const mapStateToProps = ({
   actionSuccessfully,
   listUserData,
   loadingMostPopular,
-  listMostPopular,
+  listMostPopular
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  getById: (id) => dispatch({ type: SOCIAL_MEDIA.GET_BY_ID, data: { id } }),
-  getListComment: (data) =>
+const mapDispatchToProps = dispatch => ({
+  getById: id => dispatch({ type: SOCIAL_MEDIA.GET_BY_ID, data: { id } }),
+  getListComment: data =>
     dispatch({ type: COMMENTS.GET_LIST_COMMENTS, data: { data } }),
-  addComment: (data) =>
-    dispatch({ type: COMMENTS.ADD_COMMENT, data: { data } }),
-  updateStateReducer: (data) =>
+  addComment: data => dispatch({ type: COMMENTS.ADD_COMMENT, data: { data } }),
+  updateStateReducer: data =>
     dispatch({ type: SOCIAL_MEDIA.UPDATE_SOCIAL_MEDIA_REDUCER, data }),
-  updateStateCommentReducer: (data) =>
+  updateStateCommentReducer: data =>
     dispatch({ type: COMMENTS.UPDATE_STATE_COMMENT_REDUCER, data }),
-  getListMostPopular: (data) =>
-    dispatch({ type: MOST_POPULAR.GET_LIST_POPULAR, data }),
+  getListMostPopular: data =>
+    dispatch({ type: MOST_POPULAR.GET_LIST_POPULAR, data })
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Detail);
